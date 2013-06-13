7e166111af0020ab8a79e1400e0d58c8996c8edc
Chris Bannister <c.bannister@gmail.com>
2013-06-13 12:48:09 +0100

    Rename Nttp to Client and fix the types of run and loop

--- src/Nntp/Client.hs
+++ src/Nntp/Client.hs
@@ -1,13 +1,17 @@
 {-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
 
-module NNTP.Server
+module NNTP.Client
 ( NNTPServer
+, NNTPServerT
+, fetchArticle
+, nntpMain
 ) where
 
 import Prelude hiding (catch)
 import Control.Exception
 import Control.Monad.Reader
 import Data.Maybe
+
 import Network.Socket hiding (send, sendTo, recv, recvFrom)
 import Network.Socket.ByteString
 
@@ -15,7 +19,7 @@ import System.IO.Streams (InputStream, OutputStream)
 import qualified System.IO.Streams as S
 
 import qualified Data.ByteString as B
-import qualified Data.ByteString.Char8 as Bc
+import qualified Data.ByteString.Char8 as BC
 
 import Nzb.NzbFile
 
@@ -31,15 +35,15 @@ data UsenetServerC = UsenetSSLConf
 	, port :: Int
 	}
 
-nntpMain :: UsenetServerC -> IO ()
-nntpMain conf = bracket (nntpConnect conf) (disconnect) (loop)
+nntpMain :: Nzb -> UsenetServerC -> IO (Maybe B.ByteString)
+nntpMain nzb conf = bracket (nntpConnect conf) (disconnect) (loop)
 	where
 		disconnect = sClose . nntpSocket
-		loop :: NNTPServer -> IO ()
-		loop st = runReaderT run st
+		loop :: NNTPServer -> IO (Maybe B.ByteString)
+		loop st = runReaderT (run nzb) st
 
-run :: NNTPServerT ()
-run = undefined
+run :: Nzb -> NNTPServerT (Maybe B.ByteString)
+run (Nzb _ [files]) = fetchArticle (nzbFileGroups files) (head . nzbFileSegments $ files)
 
 nntpConnect :: UsenetServerC -> IO NNTPServer
 nntpConnect (UsenetSSLConf host port) = do
@@ -50,25 +54,24 @@ nntpConnect (UsenetSSLConf host port) = do
 
 	return $ NNTPServer is os sock
 
-
---connect (UsenetConf host port) = do
+crlf :: B.ByteString -> B.ByteString
+crlf line = B.concat [line, (B.singleton 0xd), (B.singleton 0xa)]
 
 nntpSend :: B.ByteString -> NNTPServerT (Maybe B.ByteString)
 nntpSend cmd = do
 	os <- asks nntpOutput
 	is <- asks nntpInput
 
-	liftIO $ S.write (Just cmd) os
+	liftIO $ S.write (Just $ crlf cmd) os
 	liftIO $ S.read is
 
 setGroup :: B.ByteString -> NNTPServerT (Maybe B.ByteString)
 setGroup group = nntpSend (B.append "GROUP " group)
 
 getArticle :: B.ByteString -> NNTPServerT (Maybe B.ByteString)
-getArticle article = nntpSend (B.append "ARTICLE " article)
+getArticle article = nntpSend (B.append "STAT " article)
 
 fetchArticle :: [NzbGroup] -> NzbSegment -> NNTPServerT (Maybe B.ByteString)
-fetchArticle groups (NzbSegment size number article) = getArticle (Bc.pack article)
+fetchArticle groups (NzbSegment size number article) = getArticle (BC.pack article)
 	where
-		group = (Bc.pack . head) groups
-
+		group = (BC.pack . head) groups
