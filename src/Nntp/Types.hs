c0debb03d6e51462248b4d7b6d0152679a0bb19c
Chris Bannister <c.bannister@gmail.com>
2013-06-14 16:55:37 +0100

    Move NNTP Types into their own file

---
+++ src/Nntp/Types.hs
@@ -0,0 +1,25 @@
+module NNTP.Types
+( NNTPResponse
+, NNTPServerT
+, NNTPServer(..)
+) where
+
+import qualified Data.ByteString as B
+
+import System.IO.Streams (InputStream, OutputStream)
+import Control.Monad.Reader
+import Network.Socket (Socket)
+
+import Config
+
+type NNTPResponse = (Int, B.ByteString)
+
+type NNTPServerT = ReaderT NNTPServer IO
+data NNTPServer = NNTPServer
+	{ nntpInput :: InputStream B.ByteString
+	, nntpOutput :: OutputStream B.ByteString
+	, nntpSocket :: Socket
+	, nntpConfig :: ServerConfig
+	}
+
+
