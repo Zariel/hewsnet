990ac8c79540036350476b1576403eeb5c2c8299
Chris Bannister <c.bannister@gmail.com>
2013-06-14 16:55:54 +0100

    Add a basic json based config with parser

--- src/Config.hs
+++ src/Config.hs
@@ -1,12 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Config
-( NNTPServerConf(..)
+( ServerConfig(..)
 , Config(..)
 , openConfig
 ) where
 
 import System.IO
 
-data NNTPServerConf = NNTPServerConf
+import Control.Applicative
+import Control.Monad
+
+import Data.Aeson
+import Data.Aeson.Types
+import qualified Data.ByteString.Lazy as B
+
+data ServerConfig = ServerConfig
 	{ serverName :: String
 	, serverHost :: String
 	, serverPort :: Int
@@ -18,11 +26,39 @@ data NNTPServerConf = NNTPServerConf
 	} deriving (Show)
 
 data Config = Config
-	{ configServers :: [ NNTPServerConf ]
+	{ configServers :: [ ServerConfig ]
 	, configTempDir :: String
 	, configFinishDir :: String
 	, configApiPort :: Integer
 	} deriving (Show)
 
-openConfig :: FilePath -> IO Config
-openConfig path = return $ Config [] "" "" 0
+instance FromJSON Config where
+	parseJSON = parseConfig
+
+instance FromJSON ServerConfig where
+	parseJSON = parseServer
+
+parseConfig :: Value -> Parser Config
+parseConfig (Object v) = do
+	servers <- (v .: "servers") >>= parseJSON
+	tempDir <- (v .: "tempDir")
+	completeDir <- (v .: "completeDir")
+	apiPort <- (v .: "apiPort")
+
+	return $ Config servers tempDir completeDir apiPort
+
+parseServer :: Value -> Parser ServerConfig
+parseServer (Object v) =
+	ServerConfig <$>
+	(v .: "name") <*>
+	(v .: "host") <*>
+	(v .: "port") <*>
+	(v .: "username") <*>
+	(v .: "password") <*>
+	(v .: "ssl") <*>
+	(v .: "connections") <*>
+	(v .: "retention")
+
+-- Currently the config is a JSON file but in the future this may change
+openConfig :: B.ByteString -> Maybe Config
+openConfig = decode
